49e2e33414c0f3760ad0d62577d4fafd0b757e0a
FineMoss <jakedjoe@gmail.com>
2025-03-22 22:54:46 -0400

    build organization

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+PROJECT_ROOT=$(cd "$(dirname "$0")" && pwd)
+
+# Create the build directory if it does not exist
+if [ ! -d "$PROJECT_ROOT/build" ]; then
+    mkdir build
+fi
+
+cmake -S $PROJECT_ROOT/src -B $PROJECT_ROOT/build 
+cmake --build $PROJECT_ROOT/build
