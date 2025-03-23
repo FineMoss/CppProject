4b57229ad5aa39f01c5c307a3f25509f7d4d465b
FineMoss <jakedjoe@gmail.com>
2025-03-22 23:09:44 -0400

    organize docker folder

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+PROJECT_ROOT=$(cd "$(dirname "$0")" && pwd)
 
 # Build the docker image
-docker build -t cpp-env -f ./dockerfile .
+docker build -t cpp-env \
+    -f $PROJECT_ROOT/docker/dockerfile $PROJECT_ROOT/docker
