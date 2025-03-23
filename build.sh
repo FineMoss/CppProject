4b8a6c98cabeca5f947946d8faffd2f42df9efc3
FineMoss <jakedjoe@gmail.com>
2025-03-23 00:45:57 -0400

    organize build dir

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 PROJECT_ROOT=$(cd "$(dirname "$0")" && pwd)
 
 # Create the build directory if it does not exist
-if [ ! -d "$PROJECT_ROOT/build" ]; then
-    mkdir build
+BUILD_PATH="$PROJECT_ROOT/build/main"
+if [ ! -d $BUILD_PATH ]; then
+    mkdir -p $BUILD_PATH
 fi
 
-cmake -S $PROJECT_ROOT/src -B $PROJECT_ROOT/build 
-cmake --build $PROJECT_ROOT/build
+cmake -S $PROJECT_ROOT/src -B $BUILD_PATH
+cmake --build $BUILD_PATH
